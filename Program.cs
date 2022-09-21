4051dea6bb4fa4b110a5ffc8525bc87894ab1343
krobert2003 <114071981+krobert2003@users.noreply.github.com>
2022-09-21 13:10:35 +0200

    2.0

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace Githubos
     {
         static void Main(string[] args)
         {
-          
+            Console.WriteLine("Adj meg egy számot");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adj meg egy számot");
+            int b = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
